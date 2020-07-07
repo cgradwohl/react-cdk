15390308709c8f4a5953e43a9401362320c7824d
Chris Gradwohl <christophergradwohl@gmail.com>
2020-07-06 17:26:45 -0700

    provision bucket policy

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
+import * as iam from '@aws-cdk/aws-iam';
 
 export default class ReactCDKStack extends cdk.Stack {
   bucket: s3.Bucket;
@@ -8,7 +9,19 @@ export default class ReactCDKStack extends cdk.Stack {
     super(scope, id, props);
 
     this.bucket = new s3.Bucket(this, 'ReactCDKBucket', {
-      bucketName: 'react-cdk'
+      bucketName: 'react-cdk',
+      websiteIndexDocument: 'index.html',
+      blockPublicAccess: new s3.BlockPublicAccess({ restrictPublicBuckets: false })
     });
+
+    const bucketPolicy = new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [
+        `${this.bucket.bucketArn}/*`
+      ],
+      principals: [new iam.Anyone()],
+    })
+    
+    this.bucket.addToResourcePolicy(bucketPolicy);
   }
 }
