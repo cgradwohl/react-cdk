68aea43363a7c9e4b24e006d390d912fa8f14ec7
Chris Gradwohl <christophergradwohl@gmail.com>
2020-07-06 17:14:35 -0700

    get dat bucket big boi.

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 
-export class InfraStack extends cdk.Stack {
+export default class ReactCDKStack extends cdk.Stack {
+  bucket: s3.Bucket;
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.bucket = new s3.Bucket(this, 'ReactCDKBucket', {
+      bucketName: 'react-cdk'
+    });
   }
 }
